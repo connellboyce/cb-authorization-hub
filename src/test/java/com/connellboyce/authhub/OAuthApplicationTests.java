a24828a8ed2bcf0545eb41bf226a3f416230d372
Connell Boyce <connellboyce17@gmail.com>
2025-04-04 18:19:56 -0400

    test: Resolve context loading issue during unit test

--- src/test/java/com/connellboyce/authhub/OAuthApplicationTests.java
+++ src/test/java/com/connellboyce/authhub/OAuthApplicationTests.java
@@ -1,4 +1,13 @@
 package com.connellboyce.authhub;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class OAuthApplicationTests {
+	@Test
+	void contextLoads() {
+	}
 }
