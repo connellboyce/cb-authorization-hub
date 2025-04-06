42055302e4b9cd03882b06b6eb8fbc542dff6145
Connell Boyce <connellboyce17@gmail.com>
2025-04-06 19:28:37 -0400

    feat: Create basic create client API

--- src/main/java/com/connellboyce/authhub/AuthHubApplication.java
+++ src/main/java/com/connellboyce/authhub/AuthHubApplication.java
@@ -2,8 +2,10 @@ package com.connellboyce.authhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class AuthHubApplication {
 
 	public static void main(String[] args) {
