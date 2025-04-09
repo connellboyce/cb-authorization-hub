b74da3d5013c83e60cc633cbda222fe5f91d318a
Connell Boyce <connellboyce17@gmail.com>
2025-04-08 22:29:01 -0400

    feat: Create Dev Hub landing page

--- src/main/java/com/connellboyce/authhub/config/WebSecurityConfig.java
+++ src/main/java/com/connellboyce/authhub/config/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -31,14 +30,10 @@ import org.springframework.security.oauth2.server.authorization.token.OAuth2Toke
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -79,7 +74,6 @@ public class WebSecurityConfig {
 						.requestMatchers("/api/v1/user").permitAll()
 						.requestMatchers("/register").permitAll()
 						.requestMatchers("/portal/**").hasRole("DEVELOPER")
-						.requestMatchers("/portal").hasRole("DEVELOPER")
 						.anyRequest().authenticated())
 				.oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
 				.userDetailsService(userDetailsService)
